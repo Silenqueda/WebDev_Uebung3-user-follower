2550b31247ec849387e02b21ce5716fdb85abc20
s0556150 <d.b.extras@gmx.de>
2018-05-13 17:28:27 +0200

    added: Destroy function to users_controller and button to index

--- webDev_uebung_3/app/controllers/users_controller.rb
+++ webDev_uebung_3/app/controllers/users_controller.rb
@@ -10,17 +10,39 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-
     if  @user.save
     redirect_to @user
     else
       render 'new'
-    end  
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  @user = User.find(params[:id])
+
+  #hier stimmt was nicht. . . undefined method 'permit' line 32
+  if @user.update(params[:user].permit(:title, :text))
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
 
  private
   def user_params
