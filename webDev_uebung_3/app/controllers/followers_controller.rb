097f9e759dd792028e0f5b691d5d6bcd1f134c86
s0556150 <d.b.extras@gmx.de>
2018-05-13 20:25:26 +0200

    heroku deploy

--- webDev_uebung_3/app/controllers/followers_controller.rb
+++ webDev_uebung_3/app/controllers/followers_controller.rb
@@ -2,6 +2,15 @@ class FollowersController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     @follower = @user.followers.create(params[:follower].permit(:follower, :body))
+
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @follower = @user.follower.find(params[:id])
+    @follower.destroy
+
     redirect_to user_path(@user)
   end
 end
--- webDev_uebung_3/app/models/user.rb
+++ webDev_uebung_3/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :followers
+  has_many :followers, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5}
 end
